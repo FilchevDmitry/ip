18bdb34f2fb52ba4306629f4189a83f475ef6db3
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-03-02 19:51:39 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+std::string num(std::string ip,int temp){
 
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "IP" << std::endl;
+    std::string ip;
+    std::string ip1;
+    std::string ip2;
+    std::string ip3;
+    std::string ip4;
+    std::cout<<"Input IP address\n";
+    std::getline(std::cin,ip);
+    int temp=0;
+    for(int i=0;i<ip.length();i++){
+        if(ip[i]=='.')
+            temp++;
+        else {
+            if (temp == 0)
+                ip1 += ip[i];
+            if (temp == 1)
+                ip2 += ip[i];
+            if (temp == 2)
+                ip3 += ip[i];
+            if (temp == 3)
+                ip4 += ip[i];
+        }
+    }
+    std::cout<<ip1<<"\n";
+    std::cout<<ip2<<"\n";
+    std::cout<<ip3<<"\n";
+    std::cout<<ip4<<"\n";
     return 0;
 }
