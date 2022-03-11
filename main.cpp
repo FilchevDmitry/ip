840ada6d7a50e4459ec9cb2811ff916bb0166194
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-03-11 13:32:11 +0300

    ip

--- main.cpp
+++ main.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
-std::string num(std::string ip,int temp){
+bool numCheck(std::string ip)
+{
+    for (int i = 0; i < ip.length(); ++i)
+    {
+        if(ip[i]<'0' && ip[i]<'9')
+            return false;
+        else
+            return true;
+    }
 
 }
-int main() {
-    std::cout << "IP" << std::endl;
-    std::string ip;
+bool testIp(std::string ip) {
+    int temp = 0;
     std::string ip1;
     std::string ip2;
     std::string ip3;
     std::string ip4;
-    std::cout<<"Input IP address\n";
-    std::getline(std::cin,ip);
-    int temp=0;
-    for(int i=0;i<ip.length();i++){
-        if(ip[i]=='.')
+    for (int i = 0; i < ip.length(); i++) {
+        if (ip[i] == '.' && ip[i + 1] != '.')
             temp++;
         else {
-            if (temp == 0)
-                ip1 += ip[i];
-            if (temp == 1)
-                ip2 += ip[i];
-            if (temp == 2)
-                ip3 += ip[i];
-            if (temp == 3)
-                ip4 += ip[i];
+            return 0;
         }
+        if (temp == 0)
+            ip1 += ip[i];
+        if (temp == 1)
+            ip2 += ip[i];
+        if (temp == 2)
+            ip3 += ip[i];
+        if (temp == 3)
+            ip4 += ip[i];
+
     }
-    std::cout<<ip1<<"\n";
-    std::cout<<ip2<<"\n";
-    std::cout<<ip3<<"\n";
-    std::cout<<ip4<<"\n";
+    bool num1=numCheck(ip1);
+    bool num2= numCheck(ip2);
+    bool num3= numCheck(ip3);
+    bool num4= numCheck(ip4);
+    if (num1 && num2 && num3 && num4)
+        return true;
+    else
+        return false;
+}
+int main() {
+    std::cout << "IP" << std::endl;
+    std::string ip;
+    std::cout<<"Input IP address\n";
+    std::getline(std::cin,ip);
+    (testIp(ip))? std::cout<<"Yes\n" : std::cout<<"No\n";
     return 0;
 }
